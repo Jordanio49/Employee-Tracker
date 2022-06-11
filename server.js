eaca0f5b73e3b89c5852a1ebf1a05a1a2393e7e7
Jordan Thomas <jthomas135@hotmail.com>
2022-06-11 11:53:51 -0700

    included add role function and did some minor refactoring

--- server.js
+++ server.js
@@ -15,16 +15,15 @@ const connection = mysql.createConnection({
 });
 
 connection.connect(err => {
-    if (err)
-        throw err;
+    if (err) throw err;
 
-        // creating the intro page for the console
-            console.log("***********************************")
-            console.log("*                                 *")
-            console.log("*        EMPLOYEE MANAGER         *")
-            console.log("*                                 *")
-            console.log("***********************************")
-            promptUser();
+    // creating the intro page for the console
+    console.log("***********************************")
+    console.log("*                                 *")
+    console.log("*        EMPLOYEE MANAGER         *")
+    console.log("*                                 *")
+    console.log("***********************************")
+    promptUser();
 })
 
 
@@ -44,45 +43,43 @@ function promptUser() {
             "Add Department",
             "Quit"
         ]
+    }).then((answer) => {
+        // switch statement to run functions based on what option the user selected
+        switch (answer.action) {
+            case "View All Employees":
+                viewAllEmployees();
+                break;
+            case "Add Employee":
+                addEmployee();
+                break;
+            case "Update Employee Role":
+                updateEmployeeRole();
+                break;
+            case "View All Roles":
+                viewAllRoles();
+                break;
+            case "Add Role":
+                addRole();
+                break;
+            case "View All Departments":
+                viewAllDepartments();
+                break;
+            case "Add Department":
+                addDepartment();
+                break;
+            case "Quit":
+                quitApp();
+                break;
+            default:
+                break;
+        }
     })
-        .then(function (answer) {
-            // switch statement to run functions based on what option the user selected
-            switch (answer.action) {
-                case "View All Employees":
-                    viewAllEmployees();
-                    break;
-                case "Add Employee":
-                    addEmployee();
-                    break;
-                case "Update Employee Role":
-                    updateEmployeeRole();
-                    break;
-                case "View All Roles":
-                    viewAllRoles();
-                    break;
-                case "Add Role":
-                    addRole();
-                    break;
-                case "View All Departments":
-                    viewAllDepartments();
-                    break;
-                case "Add Department":
-                    addDepartment();
-                    break;
-                case "Quit":
-                    quitApp();
-                    break;
-                default:
-                    break;
-            }
-        })
 };
 
 function viewAllEmployees() {
     var query = "SELECT * FROM employees";
     connection.query(query, function (err, res) {
-        if (err)
-            throw err;
+        if (err) throw err;
         console.table(res);
         promptUser();
     })
@@ -91,15 +88,14 @@ function viewAllEmployees() {
 function addEmployee() {
     var query = "SELECT * FROM roles";
     connection.query(query, (err, res) => {
-        if (err)
-            throw err;
+        if (err) throw err;
         inquirer.prompt([
             {
                 name: "first_name",
                 type: "input",
                 message: "What is the new employee's first name?",
-                validate: firstNameInput => {
-                    if (firstNameInput) {
+                validate: Input => {
+                    if (Input) {
                         return true;
                     } else {
                         console.log('Please enter a first name for your employee');
@@ -111,8 +107,8 @@ function addEmployee() {
                 name: "last_name",
                 type: "input",
                 message: "What is the new employee's last name?",
-                validate: lastNameInput => {
-                    if (lastNameInput) {
+                validate: Input => {
+                    if (Input) {
                         return true;
                     } else {
                         console.log('Please enter a last name for your employee');
@@ -124,8 +120,8 @@ function addEmployee() {
                 name: "manager_id",
                 type: "input",
                 message: "What is the employee's manager's ID?",
-                validate: managerIdInput => {
-                    if (managerIdInput) {
+                validate: Input => {
+                    if (Input) {
                         return true;
                     } else {
                         console.log('Please enter a manager id for your employee');
@@ -143,31 +139,30 @@ function addEmployee() {
                     }
                     return rolesArr;
                 },
-                message: "What is the new employee's role"
+                message: "What is the new employee's role?"
             }
-        ])
-            .then(function (answer) {
-                let role_id;
-                for (let j = 0; j < res.length; j++) {
-                    if (res[j].title == answer.role) {
-                        role_id = res[j].id;
-                    }
+        ]).then((answer) => {
+            let role_id;
+            for (let j = 0; j < res.length; j++) {
+                if (res[j].title == answer.role) {
+                    role_id = res[j].id;
                 }
-                connection.query(
-                    "INSERT INTO employees SET ?",
-                    {
-                        first_name: answer.first_name,
-                        last_name: answer.last_name,
-                        manager_id: answer.manager_id,
-                        role_id: role_id
-                    },
-                    function (err) {
-                        if (err)
-                            throw err;
-                        promptUser();
-                    }
-                )
-            })
+            }
+            connection.query(
+                "INSERT INTO employees SET ?",
+                {
+                    first_name: answer.first_name,
+                    last_name: answer.last_name,
+                    manager_id: answer.manager_id,
+                    role_id: role_id
+                },
+                function (err) {
+                    if (err) throw err;
+                    console.table(res);
+                    promptUser();
+                }
+            )
+        })
     })
 };
 
@@ -176,26 +171,88 @@ function updateEmployeeRole() { };
 function viewAllRoles() {
     var query = "SELECT * FROM roles";
     connection.query(query, (err, res) => {
-        if (err)
-            throw err;
+        if (err) throw err;
         console.table("All Roles", res);
         promptUser();
     })
 };
 
-function addRole() { };
+function addRole() {
+    var query = "SELECT * FROM departments"
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                name: "new_role",
+                type: "input",
+                message: "What role would you like to add?",
+                validate: Input => {
+                    if (Input) {
+                        return true;
+                    } else {
+                        console.log('Please enter a new role title');
+                        return false;
+                    }
+                }
+            },
+            {
+                name: "salary",
+                type: "input",
+                message: "What is the salary for this role?",
+                validate: Input => {
+                    if (Input) {
+                        return true;
+                    } else {
+                        console.log('Please enter a salary for the new role');
+                        return false;
+                    }
+                }
+            },
+            {
+                name: 'department',
+                type: 'list',
+                choices: function () {
+                    var deptArr = [];
+                    for (let i = 0; i < res.length; i++) {
+                        deptArr.push(res[i].name);
+                    }
+                    return deptArr;
+                },
+            }
+        ]).then((answer) => {
+            let dept_id;
+            for (let j = 0; j < res.length; j++) {
+                if (res[j].name == answer.department) {
+                    dept_id = res[j].id
+                }
+            }
+            connection.query(
+                "INSERT INTO roles SET ?",
+                {
+                    job_title: answer.new_role,
+                    salary: answer.salary,
+                    dept_id: dept_id
+                },
+                function (err, res) {
+                    if (err) throw err;
+                    console.table(res);
+                    promptUser();
+                }
+            )
+        })
+    })
+};
 
 function viewAllDepartments() {
     var query = "SELECT * FROM departments";
     connection.query(query, (err, res) => {
-        if (err)
-            throw err;
+        if (err) throw err;
         console.table("All Departments", res);
         promptUser();
     })
 };
 
-function addDepartment() { };
+function addDepartment() {};
 
 function quitApp() {
     connection.end();
